3b4eae56cdb1b6efaf5ff643e7cc1e813668d438
Joan Perals <joan.perals@ixds.com>
2019-05-02 17:41:20 +0200

    Add failing e2e test

---
+++ cypress/integration/git-resume/basic-flow.js
@@ -0,0 +1,8 @@
+describe('Basic flow', () => {
+  it('fills the form and clicks on button', () => {
+    cy.visit('/')
+    cy.get('input[type="text"]').type(Cypress.env.GITHUB_USER)
+    cy.get('button').click()
+    cy.contains(Cypress.env.LANGUAGE)
+  })
+})
