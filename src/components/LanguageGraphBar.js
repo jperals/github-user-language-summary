a51bb53f1cc2ff5db928f07dc4162e01c6d37124
Joan Perals <joan.perals@ixds.com>
2019-05-03 08:34:57 +0200

    Improve layout of LanguageGraphBar

--- src/components/LanguageGraphBar.js
+++ src/components/LanguageGraphBar.js
@@ -11,13 +11,20 @@ export default function LanguageGraphBar(props) {
         transform: `scaleX(${scale})`,
         transformOrigin: 'left center'
     }
+    const statsTextStyle = {
+        left: 100 * scale + '%'
+    }
     return props.name && !isNaN(props.size) && (
-        <div className="relative mb2">
-            <div className="absolute" style={barStyle}></div>
-            <div className="relative">
-                <span>{props.name}&nbsp;</span>
-                <span>{props.size} bytes</span>
+        <div className="cf mb3">
+            <div className="fl w-20 tr">
+                <span className="mr2">{props.name}</span>
             </div>
-        </div>
+            <div className="fl w-80 relative">
+                <div className="absolute" style={barStyle}></div>
+                <div className="relative ml2" style={statsTextStyle}>
+                    <span>{props.size} bytes</span>
+                </div>
+            </div>
+        </div >
     )
 }
